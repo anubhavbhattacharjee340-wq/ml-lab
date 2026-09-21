1ac0c14e2ee4e8ac75a855d490876ea531d48e41
anubhavbhattacharjee340-wq <anubhavbhattacharjee340@gmail.com>
2026-09-21 08:59:58 +0530

    Update 1.3.py

--- ass1/ass1/1.3.py
+++ ass1/ass1/1.3.py
@@ -1,1 +1,37 @@
 
+import pandas as pd
+import numpy as np
+
+marks = np.array([72, 85, 91, 68, 77])
+
+print("marks", marks)
+print("mean", np.mean(marks))
+print("maximum:", np.max(marks))
+print("minimum:", np.min(marks))
+
+data = {
+    "Name": ["Amit", "Riya", "Sourav", "Neha", "Rahul"],
+    "Attendance": [88, 92, 76, 95, 81],
+    "Marks": [72, 85, 68, 91, 77]
+}
+
+df = pd.DataFrame(data)
+
+# Dynamically calculate Grade based on Marks
+def calculate_grade(marks):
+    if marks >= 90:
+        return "A"
+    elif marks >= 80:
+        return "B"
+    elif marks >= 70:
+        return "C"
+    elif marks >= 60:
+        return "D"
+    else:
+        return "F"
+
+# Add the new Grade column
+df["Grade"] = df["Marks"].apply(calculate_grade)
+
+print("\n--- DataFrame with Grade ---")
+print(df)
